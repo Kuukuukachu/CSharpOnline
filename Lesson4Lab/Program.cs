c4321337faab55dd32d8515f23dbfbd4ee0e252d
Lisabeth Phelps <lmphelps05@gmail.com>
2020-11-23 14:05:11 -0600

    Lesson 4 My Application Breakdown

---
+++ Lesson4Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson4Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            House myHouse = new House();
+        }
+    }
+}
