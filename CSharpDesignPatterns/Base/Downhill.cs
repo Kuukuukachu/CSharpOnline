2ceea644a068cf517180335de5b2dd8d8d3f97b5
Lisabeth Phelps <lmphelps05@gmail.com>
2020-12-19 20:07:56 -0600

    Design Patterns Base

---
+++ CSharpDesignPatterns/Base/Downhill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDesignPatterns
+{
+    public class Downhill : AbstractMountainBike
+    {
+        public Downhill(IWheel wheel, BikeColor color) 
+            :base(wheel, color) { }
+        public Downhill(IWheel wheel)
+            :this(wheel, BikeColor.Green) { }
+        public override decimal Price
+        {
+            get { return 870.00m; }
+        }
+
+    } // end class
+} // end space
---
+++ CSharpDesignPatterns/Base/NarrowWheel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDesignPatterns
+{
+    public class NarrowWheel : AbstractWheel
+    {
+        public NarrowWheel(int size) 
+            : base (size, false)
+        {
+
+        }
+
+    } // end class
+} // end namespace
